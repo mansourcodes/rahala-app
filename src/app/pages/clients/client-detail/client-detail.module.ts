9b0fec89347f5ab3a2c4eaa3cc30ccd9da283090
unknown <user@mansour.COMPSEC.EDU>
2020-03-10 11:45:35 +0300

    client details dona

--- src/app/pages/clients/client-detail/client-detail.module.ts
+++ src/app/pages/clients/client-detail/client-detail.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { ClientDetailPageRoutingModule } from './client-detail-routing.module';
 
 import { ClientDetailPage } from './client-detail.page';
+import { ClientComponentsModule } from './components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ClientDetailPageRoutingModule
+    ClientDetailPageRoutingModule,
+    ClientComponentsModule
   ],
   declarations: [ClientDetailPage]
 })
-export class ClientDetailPageModule {}
+export class ClientDetailPageModule { }
--- src/app/pages/clients/client-detail/client-detail.page.ts
+++ src/app/pages/clients/client-detail/client-detail.page.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClientsService } from 'src/app/services/providers/clients.service';
+import { Client } from 'src/app/services/models/client.model';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-client-detail',
   templateUrl: './client-detail.page.html',
   styleUrls: ['./client-detail.page.scss'],
 })
-export class ClientDetailPage implements OnInit {
+export class ClientDetailPage implements OnInit, OnDestroy {
+  client: Client;
+  private clientSub: Subscription;
+  isLoading = true;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private clientsService: ClientsService,
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('clientId')) {
+        this.navCtrl.navigateBack('/clients');
+        return;
+      }
+      this.clientSub = this.clientsService
+        .getClient(paramMap.get('clientId'))
+        .subscribe(client => {
+          this.client = client;
+          this.isLoading = false;
+        });
+    });
   }
 
+
+  ngOnDestroy() {
+    if (this.clientSub) {
+      this.clientSub.unsubscribe();
+    }
+  }
 }
