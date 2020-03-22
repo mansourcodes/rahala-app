786aeca3c5cf9ef05736c095e701ac61ba892740
unknown <user@mansour.COMPSEC.EDU>
2020-03-22 11:49:29 +0300

    try @ng-bootstrap

--- src/app/pages/settings/general-settings/general-settings.page.ts
+++ src/app/pages/settings/general-settings/general-settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-general-settings',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralSettingsPage implements OnInit {
 
-  constructor() { }
+  hoveredDate: NgbDate | null = null;
 
+  fromDate: NgbDate;
+  toDate: NgbDate | null = null;
+
+
+  constructor(calendar: NgbCalendar) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  }
   ngOnInit() {
   }
+
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+  }
+
 
 }
